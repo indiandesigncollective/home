c8521f598f5e4f0c9f8d006bf3e6f26283b8ce35
arundhati balasubramaniam <48311336+arundhati08@users.noreply.github.com>
2021-11-17 14:59:05 +0530

    updated tags

--- HomeScreen.js
+++ HomeScreen.js
@@ -6,13 +6,15 @@ import products from '../products'
 const HomeScreen = () => {
     return (
         <>
-            <h1>Bestsellers</h1>
+            <center><h2>Bestsellers</h2></center>
             <Row>
-                {products.map((product) => (
+                {products.filter(p => p.Tag === "bestseller").map(product => (
+                
                     <Col key={product._id} s={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
                      </Col>
-                ))}
+                ))
+                }
             </Row>
         </>
     )
