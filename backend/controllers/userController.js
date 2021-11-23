189a45ddd4c3adb8abdf44bd1efaae6a72ecbf2a
Medini Chopra <medini.chopra_ug23@ashoka.edu.in>
2021-11-23 23:52:23 +0530

    route for update profile works :)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,7 +76,7 @@ const getUserProfile = asyncHandler(async (req,res) => {
 //@desc Update user profile
 //@route PUT /api/users/profile
 //@access Private
-const updateProfile = asyncHandler(async (req,res) => {
+const updateUserProfile = asyncHandler(async (req,res) => {
     const user = await User.findById(req.user._id)
     if(user){
         user.name = req.body.name || user.name
