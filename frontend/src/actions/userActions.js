223944d33babd5ce50b21cc6794c1afc2c1cd0e7
arundhati08 <write2arundhati@gmail.com>
2021-11-24 00:12:23 +0530

    error :(

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -66,3 +66,43 @@ export const register = (name, email, password) => async (dispatch) => {
         })
     }
 }
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_DETAILS_REQUEST,
+        })
+
+        const { userLogin: {userInfo} } = getState
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get(
+            '/api/users/${id}', 
+            { name, email, password }, 
+            config
+            )
+
+        dispatch ({
+            type: USER_DETAILS_SUCCESS, 
+            payload: data
+        })
+
+        dispatch ({
+            type: USER_LOGIN_SUCCESS, 
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+        })
+    }
+}
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -27,3 +27,15 @@ export const userRegisterReducer = (state = { }, action) => {
         return state
     }
 }
+export const userDetailsReducer = (state = { user: { } }, action) => {
+    switch(action.type){
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload }
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload } 
+        default: 
+        return state
+    }
+}
