65c7edca3df51ca58ccd9772d37aec4357d48a44
arundhati08 <write2arundhati@gmail.com>
2021-11-23 23:31:46 +0530

    postman req

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate} from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../Components/Message'
@@ -7,12 +7,11 @@ import Loader from '../Components/Loader'
 import FormContainer from '../Components/FormContainer'
 import { login } from '../actions/userActions'
 
-const LoginScreen = ({ history }) => {
+const LoginScreen = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-
+    const navigate = useNavigate();
     const dispatch = useDispatch()
-
     const userLogin = useSelector(state => state.userLogin)
     const { loading, error, userInfo } = userLogin
 
@@ -23,8 +22,6 @@ const LoginScreen = ({ history }) => {
         e.preventDefault()
         dispatch(login(email, password))
     }
-
-
     return (
         <FormContainer>
             <h1>Sign In</h1>
@@ -43,7 +40,7 @@ const LoginScreen = ({ history }) => {
                     </Form.Control>
                 </Form.Group>
                 <br></br>
-                <Button type='submit' variant='secondary'>
+                <Button href = "" type='submit' variant='secondary'>
                     Sign In
                 </Button>
             </Form>
