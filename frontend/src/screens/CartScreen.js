76ffd56b8dd05a84b2e969bfe15fabad02895a17
Siddh Veer Bakshi <79507065+sxddhvxxr@users.noreply.github.com>
2021-11-25 02:50:12 +0530

    finished uptil lecture 46

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Link, useParams, useLocation, useNavigate } from 'react-router-dom'
+import { Link, useParams, useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../Components/Message'
 import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem, Carousel, Form} from 'react-bootstrap'
@@ -12,7 +12,7 @@ const CartScreen = ({history}) => {
     const qty = location.search ? Number(location.search.split('=')[1]) : 1
     
     const dispatch = useDispatch()
-    const navigate = useNavigate()
+
     const cart = useSelector(state => state.cart)
     const { cartItems } = cart
 
@@ -27,7 +27,7 @@ const CartScreen = ({history}) => {
         }
 
     const checkoutHandler = () => {
-        navigate('login/?redirect=shipping')
+        history.push('login/?redirect=shipping')
     }
 
     return (
@@ -36,16 +36,14 @@ const CartScreen = ({history}) => {
             <h1>Shopping Bag</h1>
             {cartItems.length === 0 ? (
             <Message>Your cart is empty  <br></br><Link to ="/">Go back to home</Link></Message>
-            ) : <Card>
-            <ListGroup variant = "flush">
+            ) : <ListGroup variant = "flush">
                 {cartItems.map(item => (
-                    
                     <ListGroup.Item key = {item.product}>
                         <Row>
                             <Col md = {3}>
                             <Image src = {item.image} alt = {item.name} fluid rounded></Image>
                             </Col>
-                            <Col md = {3}>
+                            <Col md = {4}>
                                 <Link style = {{textDecoration:'none'}} to = {`/product/${item.product}`}>{item.name}</Link>
                             </Col>
                             <Col md={2}> &#8377; {item.price}</Col>
@@ -66,10 +64,11 @@ const CartScreen = ({history}) => {
                             </Col>
                         </Row>
                     </ListGroup.Item>
+
                 )
                     
                     )}
-            </ListGroup> </Card>        
+            </ListGroup>        
         }
            </Col>
            <Col md = {4}>
@@ -77,10 +76,7 @@ const CartScreen = ({history}) => {
                 <ListGroup variant = 'flush'>
                     <ListGroup.Item>
                         <h2>Subtotal ({cartItems.reduce((acc, item)=> acc + item.qty, 0)}) items</h2>
-
-                        <h5 style = {{fontFamily: 'sans-serif', fontSize: '15px'}}>Subtotal: &emsp; &#8377; {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)} </h5>
-                        <h5 style = {{fontFamily: 'sans-serif', fontSize: '15px'}}>Shipping &emsp; &#8377; 99</h5>
-                        <h4>Total: {cartItems.reduce((acc, item) => acc +99+ item.qty * item.price, 0).toFixed(2)}</h4>
+                        &#8377; {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
                     </ListGroup.Item>
                     <ListGroup.Item>
                         <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler}>
