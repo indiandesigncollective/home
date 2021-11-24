dcece0eee1962a207584040fa8d9df892c54c4c8
Siddh Veer Bakshi <siddh.bakshi_ug23@ashoka.edu.in>
2021-11-24 19:07:22 +0530

    login not working

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_SUCCESS, USER_REGISTER_REQUEST } from "../constants/userConstants"
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_SUCCESS, USER_REGISTER_REQUEST, USER_DETAILS_REQUEST, USER_DETAILS_FAIL, USER_DETAILS_SUCCESS } from "../constants/userConstants"
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -83,8 +83,7 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
         }
 
         const { data } = await axios.get(
-            '/api/users/${id}', 
-            { name, email, password }, 
+            `/api/users/${id}`,
             config
             )
 
@@ -92,16 +91,9 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
             type: USER_DETAILS_SUCCESS, 
             payload: data
         })
-
-        dispatch ({
-            type: USER_LOGIN_SUCCESS, 
-            payload: data
-        })
-
-        localStorage.setItem('userInfo', JSON.stringify(data))
     } catch (error) {
         dispatch({
-            type: USER_REGISTER_FAIL,
+            type: USER_DETAILS_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message,
         })
     }
