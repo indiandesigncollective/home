8779372f3bf897e3c414d85e59d78e8fd7f88c83
arundhati08 <write2arundhati@gmail.com>
2021-11-26 17:41:08 +0530

    Categories cards on Home

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,13 +5,16 @@ import Message from '../Components/Message'
 import Loader from '../Components/Loader'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProducts } from '../actions/productActions'
+import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
 import products from '../products'
+import CartScreen from './CartScreen'
 
 const HomeScreen = () => {
     const dispatch = useDispatch()
     const productList = useSelector(state => state.productList)
     const { loading, error, products } = productList
-    
+    const pathname = window.location.pathname
 
     useEffect(() => {
         dispatch(listProducts())
@@ -29,6 +32,51 @@ const HomeScreen = () => {
                     ))}
                 </Row>
             } 
+            <br></br>
+            <center><h2>Shop By Category</h2></center>
+            <br></br>
+            <Row>
+                <Col>
+                    <Card style={{ width: '20rem' }}>
+                    <Link style = {{textDecoration:'none'}} to= {"/decor"}>
+                        <Card.Img variant="top" src="https://media.chumbak.com/media/catalog/product/8/9/8907605103902_top.jpg" />
+                        </Link> 
+                        <Card.Body>
+                        <Link style = {{textDecoration:'none'}} to= {"/decor"}>
+                            <center>
+                                <Card.Title>Decor</Card.Title>
+                            </center>
+                            
+                            </Link> 
+                        </Card.Body>
+                        </Card>
+                 </Col>  
+                 <Col>
+                    <Card style={{ width: '20rem' }}>
+                    <Link style = {{textDecoration:'none'}} to= {"/beauty"}>
+                        <Card.Img variant="top" src="https://media.chumbak.com/media/catalog/product/image/500x625/8/9/8907605102042_plus35.jpg" />
+                     </Link> 
+                        <Card.Body>
+                        <Link style = {{textDecoration:'none'}} to= {"/beauty"}>
+                            <center><Card.Title>Beauty</Card.Title></center>
+                        </Link>    
+                        </Card.Body>
+                        </Card>
+                 </Col>  
+                 <Col>
+                    <Card style={{ width: '20rem' }}>
+                    <Link style = {{textDecoration:'none'}} to= {"/accessories"}>
+                        <Card.Img variant="top" src="https://media.chumbak.com/media/catalog/product/image/500x625/8/9/8907605098697_plus24.jpg" />
+                        </Link> 
+                        <Card.Body>
+                        <Link style = {{textDecoration:'none'}} to= {"/accessories"}>
+                            <center><Card.Title>Accessories</Card.Title></center>
+                            </Link> 
+                        </Card.Body>
+                        </Card>
+                 </Col>       
+            </Row>
+            
         </>
     )
 }
