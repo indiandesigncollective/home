69d103a1bc7b23c94aebb2ceab12e3a80c500f1a
arundhati08 <write2arundhati@gmail.com>
2021-12-06 16:15:00 +0530

    filter, fuzzy search, sort done

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -20,20 +20,36 @@ const SearchScreen = () => {
         dispatch(listProducts())
     }, [dispatch])
 
-    const [query, updateQuery] = useState('');
+    const [query, updateQuery] = useState('')
+    const [category, setCategory] = useState('All Categories')
+    const [sort, setSort] = useState('')
     const fuse = new Fuse(products, {
         keys: [
-          'Name', 'Description'
+          'Name'
         ]
       })
    
     const results = fuse.search(query)
     const productResults = query ? results.map(product => product.item) : products
+    const catResults = (category === "All Categories") ? productResults : productResults.filter(p => p.Category === category.charAt(0).toLowerCase()+category.slice(1))
+    let sortResult = catResults
+    if (sort === "Sort By: Default") {
+         sortResult = catResults
+    }
+    else if (sort === "Sort By: Low to High"){
+        // sortResult = (catResults.Price).sort()
+        sortResult = catResults.sort((firstItem, secondItem) => firstItem.Price - secondItem.Price)
+    }
+    else {
+        // sortResult = catResults.Price.sort().reverse()
+        const temp = catResults.sort((firstItem, secondItem) => firstItem.Price - secondItem.Price)
+        sortResult = catResults.reverse()
+    }
 
+    console.log(productResults)
      const onSearch = ({ currentTarget }) => {
         updateQuery(currentTarget.value);
       }
-
     return (
         <>
         <center><h1>Shop All Products</h1></center>
@@ -45,9 +61,31 @@ const SearchScreen = () => {
           onChange = {onSearch}>
           </Form.Control>
           </Form>
+
+          <br></br>
+
+          <Row>
+          <Col md = {3}>
+          <Form.Select value = {category} onChange={(e) => setCategory(e.target.value)}>
+            <option>All Categories</option>  
+            <option>Accessories</option>
+            <option>Beauty</option>
+            <option>Decor</option>
+        </Form.Select>
+          </Col>
+          <Col md = {3}></Col>
+          <Col md = {3}></Col>
+          <Col md = {3}>
+          <Form.Select value = {sort} onChange={(e) => setSort(e.target.value)}>
+            <option>Sort By: Default</option>  
+            <option>Sort By: Low to High</option>
+            <option>Sort By: High to Low</option>
+        </Form.Select>
+          </Col>
+          </Row>
         {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : 
                 <Row>
-                    {productResults.map(product => (
+                    {sortResult.map(product => (
                         <Col key={product._id} s={12} md={6} lg={4} xl={3}>
                             <Product product={product} />
                         </Col>
